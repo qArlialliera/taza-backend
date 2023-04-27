e8da45029d50350413642f5704fb58585b00856e
petiveriaalliacea <77691894+petiveriaalliacea@users.noreply.github.com>
2023-04-27 15:53:48 +0600

    Chat cors

--- src/main/java/com/petiveriaalliacea/taza/rest/ChatController.java
+++ src/main/java/com/petiveriaalliacea/taza/rest/ChatController.java
@@ -10,8 +10,10 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Controller
+@CrossOrigin(allowedHeaders = "*", origins = "*")
 public class ChatController {
 
     @Autowired
